88fd5b9cbe830ae4575fec3798fe8e6fb080bcf7
Andrew McGee <andrew.mcgee@bglgroup.co.uk>
2022-01-05 09:56:20 +0000

    Made resolving lazy

--- Sources/Injextor/DependencyWrappers/SingletonDependency.swift
+++ Sources/Injextor/DependencyWrappers/SingletonDependency.swift
@@ -11,18 +11,23 @@ import Foundation
 @propertyWrapper
 public struct SingletonDependency<T> {
     
-    /// Hold an instance of the singleton dependency.
-    private var value: T
+    /// Stores the resolver to be used to resolve the dependency.
+    private let resolver: ResolverType
+    
+    /// Hold an instance of the singleton dependency. Lazy so it is not resolved until it is first used. If it is set to a custom value before it is resolved, then it will never be resolved.
+    private lazy var value: T = {
+        resolver.resolve()
+    }()
 
     /// Initializes an instance of a `Dependency` using the given `ResolverType`. If none is provided then the default singleton resolver is used.
-    /// - Parameter resolver: A `ResolverType` usef to resolve the dependency. If none is provided then the default singleton resolver is used.
+    /// - Parameter resolver: A `ResolverType` used to resolve the dependency. If none is provided then the default singleton resolver is used.
     public init(resolver: ResolverType = Resolvers.shared.singletons) {
-        value = resolver.resolve()
+        self.resolver = resolver
     }
     
     /// Provides the dependency as the property wrapper's wrapped value.
     public var wrappedValue: T {
-        get {
+        mutating get {
             value
         }
         set {
